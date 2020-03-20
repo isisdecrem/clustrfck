2bd93c8b7c6114cc83d488ff912533ea6d9ff19e
isisdecrem <60768505+isisdecrem@users.noreply.github.com>
2020-03-20 16:09:51 -0700

    Add files via upload

---
+++ Clustr/JoinAndBrowseClubs.swift
@@ -0,0 +1,30 @@
+//
+//  JoinAndBrowseClubs.swift
+//  Clustr
+//
+//  Created by Isis Decrem on 3/20/20.
+//  Copyright © 2020 Isis Decrem. All rights reserved.
+//
+
+import UIKit
+
+class JoinAndBrowseClubs: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
